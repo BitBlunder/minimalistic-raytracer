6bc9dd6d4c24b68cd1cd2e7c836a4c2fd0502ff1
BitBlunder <fudgeracoon@gmail.com>
2025-04-01 00:27:34 +0200

    Implement PPM gradient image generation sample

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,12 +1,10 @@
+#include <cstring>
 #include <iostream>
+
+#include <type_traits>
 
 #include <Image/Image.hpp>
 #include <Image/PPMHandler.hpp>
-
-uint8_t data[] = {
-	255,   0,  0,   0, 255,   0,  0,  0, 255,
-	255, 255,  0, 255, 255, 255,  0,  0,   0
-};
 
 int main(int argc, char** args)
 {
@@ -18,13 +16,29 @@ int main(int argc, char** args)
 	Image* img = image_new();
 
 	img->format = Image::Format::FMT_RGB24;
-	img->filename = "../Assets/test.ppm";
-	img->width = 3; img->height = 2;
+	img->filename = "../Assets/sample.ppm";
+	img->width = 300; img->height = 300;
 
-	if (!image_set_buffer(img, data))
+	uint8_t* buffer = (uint8_t*)malloc(img->width * img->height * 3);
+
+	for (int y = 0; y < img->height; y++)
+		for (int x = 0; x < img->width; x++)
+		{
+			float t = static_cast<float>(x) / (img->width - 1);
+
+			uint8_t r = static_cast<uint8_t>(t * 255);
+			uint8_t g = static_cast<uint8_t>(t * 255);
+			uint8_t b = 255;
+
+			int index = (x + (y * img->width)) * 3;
+
+			buffer[index + 0] = r;
+			buffer[index + 1] = g;
+			buffer[index + 2] = b;
+		}
+
+	if (!image_set_buffer(img, buffer))
 		std::cerr << "Error occured while setting pixels to PPM file from memory\n";
-
-	image_set_pixel3i(img, 0, 0, 100, 200, 50);
 
 	if (!image_save_file(img))
 		std::cerr << "Error occured while saving PPM file to disk\n";
